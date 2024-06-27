70845d7c42f0ce91ad177cdcfa3e453d0d56d7e8
Js-cook <jscook1001@gmail.com>
2024-06-27 15:12:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function generateRandomNumber(topBound){
+    return Math.floor(Math.random() * topBound) + 1
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const titleText = document.getElementById("title")
+    const lyricText = document.getElementById("lyric-text")
+    const img = document.querySelector("img")
+    const artistText = document.getElementById("artist")
+  
+    document.addEventListener("click", () => {
+      console.log("click")
+      fetch("/data.json")
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data)
+        const lyricNumber = Object.keys(data).length
+        let selectedSong = data[Object.keys(data)[generateRandomNumber(lyricNumber)-1]]
+        console.log(selectedSong)
+  
+        titleText.innerHTML = selectedSong.title
+        lyricText.innerHTML = selectedSong.lyrics
+        artistText.innerHTML = selectedSong.artist
+        img.src = selectedSong.imgSrc
+      })
+    })
+    document.querySelector("body").click()
+  }) 
